3b5b323edeef6f2b9318aa2c27b455911314e6ff
leonardo.janardi <leojanardi@gmail.com>
2019-05-10 00:38:26 -0300

    Aula finalizada @estilizando Welcome

--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -1,12 +1,30 @@
 import React from 'react';
 
-import { View, Text } from 'react-native';
+import {
+  View, Text, TextInput, TouchableOpacity, StatusBar,
+} from 'react-native';
 
-import { Styles } from './styles';
+import styles from './styles';
 
 const Welcome = () => (
-  <View style={Styles.container}>
-    <Text>Teste</Text>
+  <View style={styles.container}>
+    <StatusBar barStyle="light-content" />
+    <Text style={styles.title}>Bem Vindo</Text>
+    <Text style={styles.text}>Para continuar precisamos que você informe seu usuário</Text>
+
+    <View style={styles.form}>
+      <TextInput
+        style={styles.input}
+        autoCapitalize="none"
+        autoCorrect={false}
+        placeholder="Digite seu usuário"
+        underlineColorAndroid="transparent"
+      />
+
+      <TouchableOpacity style={styles.button} onPress={() => {}}>
+        <Text style={styles.buttonsText}>Prosseguir</Text>
+      </TouchableOpacity>
+    </View>
   </View>
 );
 
